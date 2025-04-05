ea19b5f33fa1e1f1acb410bddba4b7a59add1e6f
hexne <printfne@gmail.com>
2025-04-05 19:55:15 +0800

    初始化界面

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QDebug>
+
+#include "MainWindow.h"
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
+    return app.exec();
+}
