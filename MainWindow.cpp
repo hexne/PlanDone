2f17c97b96393df3afe879d7abf17d0195d8cb04
hexne <printfne@gmail.com>
2025-04-14 15:49:08 +0800

    添加日历的构造

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -12,18 +12,26 @@
 #include <QJsonDocument>
 #include <QFile>
 #include <ranges>
+#include <chrono>
+#include <iostream>
 
 struct Plan {
     QString plan_name{};
 };
 
-class Data {
-    int y{}, m{}, d{};
+struct Date {
+    int year{}, month{}, day{};
     std::vector<std::shared_ptr<Plan>> done_plans;  // 每日完成的计划列表
 
-public:
-    bool operator==(const Data &other) const {
-        return d == other.d && m == other.m && y == other.y;
+    Date(int y, int m, int d) : year(y), month(m), day(d) {  }
+    Date(std::chrono::time_point<std::chrono::system_clock> time_point) {
+        auto ymd = std::chrono::year_month_day{std::chrono::floor<std::chrono::days>(time_point)};
+        year = ymd.year().operator int();
+        month = ymd.month().operator unsigned();
+        day = ymd.day().operator unsigned();
+    }
+    bool operator==(const Date &other) const {
+        return day == other.day && month == other.month && year == other.year;
     }
 
     bool find(const QString &plan_name) const {
@@ -31,14 +39,22 @@ public:
             return plan->plan_name == plan_name;
         });
     }
+
+    static int count_month_day(int year, int month) {
+        if (month == 2) {
+            return (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0) ? 29 : 28;
+        }
+        constexpr int days[] = {0, 31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return days[month];
+    }
 };
 
 class Calendar {
-    using Month = std::vector<Data>;
+    using Month = std::vector<Date>;
     using Year = std::vector<Month>;
     std::vector<Year> calendar_;
 public:
-    Data &find(const Data &date) {
+    Date &find(const Date &date) {
         for (auto &day : calendar_ | std::ranges::views::join | std::ranges::views::join) {
             if (date == day)
                 return day;
@@ -47,8 +63,32 @@ public:
     }
 
 
-    // 创建2025.1.1 到 今天 的日历
     Calendar() {
+        Date end_time = std::chrono::system_clock::now();
+
+        for (int year = 2025; year <= end_time.year; year++) {
+            Year push_year;
+            for (int month = 1; month < 12; month++) {
+                Month push_month;
+                if (year == end_time.year && month > end_time.month)
+                    break;
+                for (int day = 1; day <= Date::count_month_day(year, month); day++) {
+                    if (year == end_time.year && month == end_time.month && day > end_time.day)
+                        break;
+                    Date date {year, month, day};
+                    push_month.push_back(date);
+                }
+                push_year.push_back(push_month);
+            }
+            calendar_.push_back(push_year);
+        }
+    }
+
+    void print() {
+
+        for (const auto &day : calendar_ | std::ranges::views::join | std::ranges::views::join) {
+            std::cout << day.year << " " << day.month << " " << day.day << std::endl;
+        }
 
     }
 
@@ -160,6 +200,9 @@ MainWindow::MainWindow(QWidget *parent) :
             }
         }
     });
+
+    Calendar calendar;
+    calendar.print();
 
 
 }
